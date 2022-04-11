c5fa944900b059e74db0c4d1b5c27ab6efd49182
Joe Corrales <corralesjoe07@gmail.com>
2022-04-10 21:10:38 -0600

    second commit

---
+++ notas.ts
@@ -0,0 +1,6 @@
+// Con npm install typescript --save-dev
+// Instalas typescript de manera para solo el proyecto
+// Y no global, queriendo decir que es como que lo alquilas
+
+// npx tsc --version
+// es para saber que version de typescript descargamos
